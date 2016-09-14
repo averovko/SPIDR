16e20d37e665f7cfb86d4706ee2833897419c3b7
Oleksandr Verovko <averovko@gmail.com>
2016-09-15 00:32:16 +0300

    App launcher implementing. Issue #3

--- launcher/urls.py
+++ launcher/urls.py
@@ -5,10 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='launcher_main'), name='launcher_home'),
     url(r'^main/$', views.main, name='launcher_main'),
-<<<<<<< HEAD
     url(r'^(?P<app_name>[^/]+)/$', views.launch_app, name='launch_app'),
     url(r'^(?P<app_name>[^/]+)/(?P<dashboard_name>[^/]+)/$', views.launch_app_dashboard, name='launch_app_dashboard'),
-=======
-    url(r'^(?P<app_name>[^/]+)/$', views.launch_app),
->>>>>>> origin/master
+
 ]
--- launcher/views.py
+++ launcher/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
-<<<<<<< HEAD
 from django.conf import settings
 from .models import Application
 from .xmlconverter import XMLConverter
-=======
-from .models import Application
->>>>>>> origin/master
 
 import os
 try:
@@ -17,23 +13,16 @@ except:
 
 @login_required
 def main(request):
-<<<<<<< HEAD
-    ext_apps_dir = getattr(settings, "EXTERNAL_APPS_DIR", None)
-    apps = os.listdir(ext_apps_dir)
-=======
+
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     apps_dir = os.path.join(base_dir, 'apps')
     apps = os.listdir(apps_dir)
->>>>>>> origin/master
 
     config = configparser.ConfigParser()
     applications = []
     for app in apps:
-<<<<<<< HEAD
-        app_home = os.path.join(ext_apps_dir, app)
-=======
+
         app_home = os.path.join(apps_dir, app)
->>>>>>> origin/master
         config_file = os.path.join(app_home, 'config', 'app.conf')
 
         config.read(config_file)
@@ -43,7 +32,6 @@ def main(request):
 
     return render(request, 'launcher/main.html', {'applications': applications})
 
-<<<<<<< HEAD
 
 @login_required
 def launch_app(request, app_name):
@@ -55,7 +43,7 @@ def launch_app(request, app_name):
     config.read(config_file)
     dashboard = config.get('ui', 'default_dashboard')
 
-    return redirect('launch_app_dashboard', app_name=app_name, dashboard_name=dashboard) #render(request, 'launcher/main.html', {})
+    return redirect('launch_app_dashboard', app_name=app_name, dashboard_name=dashboard)
 
 
 @login_required
@@ -64,9 +52,4 @@ def launch_app_dashboard(request, app_name, dashboard_name):
 
     menu_html = XMLConverter.convert_navigation(xml_file=os.path.join(getattr(settings, "EXTERNAL_APPS_DIR", None), app_name, 'views', 'navigation', 'navigation.xml'))
     return render(request, 'launcher/main.html', {'menu_html': menu_html})
-=======
-@login_required
-def launch_app(request, app_name):
-    print(app_name)
-    return render(request, 'launcher/main.html', {})
->>>>>>> origin/master
+
